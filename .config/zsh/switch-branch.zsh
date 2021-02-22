50d3deaa95b35ad27cb7ac33305e3a5a1ae539d0
Evy Bongers <evy@evybongers.nl>
2021-02-22 21:53:12 +0100

    Update switch_branch

--- .config/zsh/switch-branch.zsh
+++ .config/zsh/switch-branch.zsh
@@ -1,7 +1,11 @@
 function switch_branch() {
     local current_branch local_branch target_branch
 
-    current_branch="$(home rev-parse --abbrev-ref HEAD)"
+    current_branch="$(home branch --show-current)"
+    if [[ -z "${current_branch}" ]]; then
+        echo "Currently in detached head" >&2
+        return 1
+    fi
 
     if command -v hostname &>/dev/null; then
         local_branch="$(hostname -s)"
@@ -9,9 +13,7 @@ function switch_branch() {
         local_branch="$(hostnamectl --static)"
     fi
 
-    if [[ -n "${1:-}" ]]; then
-        local_branch="${1}"
-    fi
+    local_branch="${1:-$local_branch}"
     if [[ "${local_branch:-}" == "" ]]; then
         echo "Failed to determine local branch" >&2
         return 1
@@ -21,7 +23,7 @@ function switch_branch() {
         target_branch="${local_branch}"
         # Create the local branch if it does not exist
         home branch --list "${local_branch}" |grep -q "${local_branch}" ||
-            git --git-dir="${HOME}/.files.git" --work-tree="${HOME}" branch "${target_branch}"
+            home branch "${target_branch}"
     else
         target_branch="main"
     fi
