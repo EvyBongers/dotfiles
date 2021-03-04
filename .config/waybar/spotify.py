d5b18a9005307f4a28a208e121d361513dd7b309
Evy Bongers <evy@evybongers.nl>
2021-03-04 19:50:32 +0100

    Use json output format for spotify.py

--- .config/waybar/spotify.py
+++ .config/waybar/spotify.py
@@ -2,6 +2,10 @@
 from datetime import timedelta
 from time import sleep
 import typing
+from gi.repository import GLib
+import signal
+import sys
+import json
 
 from pydbus import SessionBus
 
@@ -10,39 +14,62 @@ DBUS_CLIENT: typing.Final[str] = "org.mpris.MediaPlayer2.spotify"
 PLAYER_IFACE: typing.Final[str] = "org.mpris.MediaPlayer2.Player"
 OBJECT_PATH: typing.Final[str] = "/org/mpris/MediaPlayer2"
 
+
+def printUpdate(metadata, playbackStatus):
+    album = metadata.get("xesam:album")
+    artist = metadata.get("xesam:artist")[0]
+    microtime = metadata.get("mpris:length")
+    title = metadata.get("xesam:title")
+    print(
+        json.dumps(
+            {
+                "text": FORMAT.format(
+                    artist=artist,
+                    album=album,
+                    playback=playbackStatus,
+                    time=str(timedelta(seconds=microtime // 1_000_000)),
+                    title=title,
+                ),
+            }
+        ),
+    )
+
+
 def main():
     bus = SessionBus()
+
+    #loop = GLib.MainLoop()
+    #def signalHandler(sig, frame):
+    #    loop.quit()
+
+    #signal.signal(signal.SIGINT, signalHandler)
+    #signal.signal(signal.SIGTERM, signalHandler)
+
     try:
-        busObject = bus.get(DBUS_CLIENT, OBJECT_PATH)
-        player = busObject[PLAYER_IFACE]
+        spotify = bus.get(DBUS_CLIENT, OBJECT_PATH)
+        player = spotify[PLAYER_IFACE]
 
         try:
             metadata = player.Metadata
-            album = metadata.get("xesam:album")
-            artist = metadata.get("xesam:artist")[0]
-            microtime = metadata.get("mpris:length")
-            title = metadata.get("xesam:title")
+            playbackStatus = player.PlaybackStatus
+            printUpdate(metadata, playbackStatus)
 
-            playback_status = player.PlaybackStatus
-
-            print(
-                FORMAT.format(
-                    artist=artist,
-                    album=album,
-                    playback=playback_status,
-                    time=str(timedelta(seconds=microtime // 1_000_000)),
-                    title=title,
-                )
-            )
         except Exception:
             # Playpack is stopped
 
             # TODO: find what data is available
             pass
 
+        #spotify.PropertiesChanged.connect(
+        #    lambda _s, _a, _as: printUpdate(_a["Metadata"], _a["PlaybackStatus"])
+        #)
+        #loop.run()
+
     except Exception:
         # Spotify isn't running
+        print("Spotify isn't running!", file=sys.stderr)
         pass
+
 
 if __name__ == "__main__":
     main()
