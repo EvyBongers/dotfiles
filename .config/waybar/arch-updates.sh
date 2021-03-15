11b4dd540d4774d9b490517338638aed7727a140
Evy Bongers <evy@evybongers.nl>
2021-03-15 21:44:31 +0100

    Simplify arch-updates

--- .config/waybar/arch-updates.sh
+++ .config/waybar/arch-updates.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
 # Dependencies:
-# - makepkg utils (provided by pacman)
 # - wc (provided by coreutils)
 # - checkupdates (provided by pacman-contrib)
 # - yay (optional, for checking AUR packages)
@@ -10,11 +9,7 @@ set -o pipefail
 set -o errexit
 set -o nounset
 
-# parseopts.sh is provided by the pacman package
-[[ -r /usr/share/makepkg/util/parseopts.sh ]] &&
-    source /usr/share/makepkg/util/parseopts.sh
-
-function check_updates() {
+function main() {
     local num_repo_upgrades num_aur_upgrades
     num_repo_upgrades=$( (checkupdates |wc -l) || exit 0)
     if (command -v yay && yay --version) &>/dev/null; then
@@ -30,38 +25,6 @@ function check_updates() {
     fi
 }
 
-function main() {
-    check_updates
-    if [[ ${WATCH:-0} == 1 ]]; then
-        while true; do
-            sleep "${TIMEOUT:-600}"
-            check_updates
-        done
-    fi
-}
-
-trap 'kill $(jobs -p)' SIGCHLD
-
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    OPT_SHORT='wt:'
-    OPT_LONG=('watch', 'timeout')
-    parseopts "${OPT_SHORT}" "${OPT_LONG[@]}" -- "${@}" || exit 1
-
-    set -- "${OPTRET[@]}";
-    unset OPTRET OPT_LONG OPT_SHORT
-    while :; do
-        case "${1}" in
-            -w|--watch)
-                WATCH=1 ;;
-            -t|--timeout)
-                TIMEOUT=${2}
-                shift ;;
-            --)
-                shift
-                break ;;
-        esac
-        shift
-    done
-
     main
 fi
