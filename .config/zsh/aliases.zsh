5f83afca3552b1ec7843fe2db340db51bb45658e
Evy Bongers <evy@evybongers.nl>
2021-09-30 13:20:39 +0200

    Add some aliases for dotfiles management

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -2,10 +2,12 @@
 alias home="git --work-tree=${HOME} --git-dir=${HOME}/.files.git"
 alias ha='home add'
 alias hc='home commit -v'
+alias hc!='home commit -v --amend'
 alias hca='home commit -v --all'
 alias hd='home diff'
 alias hl="home pull"
 alias hlol="home log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+alias hm='home merge main'
 alias hp='home push'
 alias hpf='home push --force-with-lease'
 alias hrb='home rebase'
