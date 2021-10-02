7c1b7be6ab358a03b58cbc101223e4f36d6be7fc
Evy Bongers <evy@evybongers.nl>
2021-10-02 21:54:36 +0200

    Split up function logic for switching branches

--- .config/zsh/switch-branch.zsh
+++ .config/zsh/switch-branch.zsh
@@ -1,39 +1,42 @@
-function get_short_hostname() {
-    if command -v hostnamectl &>/dev/null; then
-        hostnamectl --static 2>/dev/null && return
-    fi
-    if command -v sysctl &>/dev/null; then
-        sysctl -n kernel.hostname 2>/dev/null && return
-    fi
-    if command -v hostname &>/dev/null; then
-        hostname -s 2>/dev/null && return
-    fi
+function home_local_branch() {
+    declare -a cmds=(
+        "hostnamectl --static"
+        "sysctl -n kernel.hostname"
+        "hostname -s"
+    )
+    for cmd in "${cmds[@]}"; do
+        command -v "${cmd%% *}" &>/dev/null || continue
+        eval "${cmd}" 2>/dev/null && return
+    done
+}
+function home_current_branch() {
+    declare -a cmds=(
+        "home branch --show-current"
+        "home symbolic-ref --quiet HEAD |sed 's@^refs/heads/@@'"
+    )
+    for cmd in "${cmds[@]}"; do
+        eval "${cmd}" 2>/dev/null && return
+    done
 }
 function hsw() {
     local current_branch local_branch target_branch
 
-    declare -r current_branch="$(home branch --show-current 2>/dev/null || 
-        home symbolic-ref --quiet HEAD |sed 's@^refs/heads/@@')"
+    declare -r current_branch="$(home_current_branch)"
+    declare -r local_branch="${1:-$(home_local_branch)}"
+
     if [[ -z "${current_branch:-}" ]]; then
         echo "Currently in detached head" >&2
         return 1
-    fi
-
-    declare -r local_branch="${1:-$(get_short_hostname)}"
-    if [[ -z "${local_branch:-}" ]]; then
+    elif [[ -z "${local_branch:-}" ]]; then
         echo "Failed to determine local branch" >&2
         return 1
-    fi
-
-    if [[ "${current_branch}" == "main" ]]; then
+    elif [[ "${current_branch}" == "main" ]]; then
         target_branch="${local_branch}"
-        # Create the local branch if it does not exist
-        home branch --list "${local_branch}" |grep -q "${local_branch}" ||
-            home branch "${target_branch}"
     else
         target_branch="main"
     fi
 
     echo "switching to branch '${target_branch}'"
-    home switch "${target_branch}"
+    home switch "${target_branch}" 2>/dev/null ||
+        home switch -c "${target_branch}"
 }
