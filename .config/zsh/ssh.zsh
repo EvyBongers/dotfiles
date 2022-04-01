1da0247707c3c6257747dfbb4eb5816b115ef453
Evy Bongers <evy@evybongers.nl>
2022-04-01 16:19:17 +0200

    Drop port argument in ssh convenience functions

--- .config/zsh/ssh.zsh
+++ .config/zsh/ssh.zsh
@@ -6,23 +6,20 @@ alias sshfs="sshfs -F ${XDG_CONFIG_HOME}/ssh/config"
 
 function () {
     local -r known_hosts_file=${XDG_STATE_HOME}/ssh/known_hosts
-    local fqdn port_arg
     if [[ ! -r "${known_hosts_file}" ]]; then
         return
     fi
 
     while read -r line; do
-        unset fqdn port_arg
-        fqdn="$(echo "${line%%,*}" |cut -d' ' -f1)"
+        local fqdn="$(echo "${line%%,*}" |cut -d' ' -f1)"
         if [[ "${fqdn}" == *:* ]]; then
-            port_arg="-p ${fqdn##*:} "
             fqdn="${fqdn%%:*}"
         fi
         if [[ "${fqdn:0:1}" == "[" ]]; then
             fqdn="${fqdn:1:-1}"
         fi
 
-        fn="function ${fqdn}() { ${pre_exec:-} local opts=\"\"; while [[ \$# -ge 1 ]]; do next_word=\"\$1\"; shift; if [[ \$\"\${next_word}\" != \$\"--\" ]]; then opts=\"\${opts}\${opts:+ }\$next_word\"; else break; fi; done; local args=\"\$*\"; comm=\"ssh ${port_arg:-}\${opts} ${fqdn} \${args}\"; eval \"\${comm}\"; }"
+        fn="function ${fqdn}() { ${pre_exec:-} local opts=\"\"; while [[ \$# -ge 1 ]]; do next_word=\"\$1\"; shift; if [[ \$\"\${next_word}\" != \$\"--\" ]]; then opts=\"\${opts}\${opts:+ }\$next_word\"; else break; fi; done; local args=\"\$*\"; comm=\"ssh \${opts} ${fqdn} \${args}\"; eval \"\${comm}\"; }"
         eval "${fn}"
     done < "${known_hosts_file}"
 }
