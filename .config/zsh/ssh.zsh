1551f8ded57f5407ed12b45ea833797fa56fe27f
Evy Bongers <evy@evybongers.nl>
2022-04-05 20:03:16 +0200

    Improve parsing of known hosts file

--- .config/zsh/ssh.zsh
+++ .config/zsh/ssh.zsh
@@ -11,12 +11,9 @@ function () {
     fi
 
     while read -r line; do
-        local fqdn="$(echo "${line%%,*}" |cut -d' ' -f1)"
-        if [[ "${fqdn}" == *:* ]]; then
-            fqdn="${fqdn%%:*}"
-        fi
-        if [[ "${fqdn:0:1}" == "[" ]]; then
-            fqdn="${fqdn:1:-1}"
+        local fqdn="${line%%[ ,]*}"
+        if [[ "${fqdn}" =~ ":[0-9]+$" ]]; then
+            fqdn="${${fqdn%%:*}:1:-1}"
         fi
 
         fn="function ${fqdn}() { ${pre_exec:-} local opts=\"\"; while [[ \$# -ge 1 ]]; do next_word=\"\$1\"; shift; if [[ \$\"\${next_word}\" != \$\"--\" ]]; then opts=\"\${opts}\${opts:+ }\$next_word\"; else break; fi; done; local args=\"\$*\"; comm=\"ssh \${opts} ${fqdn} \${args}\"; eval \"\${comm}\"; }"
