a82f0d15aa69d8a1ea0385d703958ec9e37fcb32
Evy Bongers <evy@evybongers.nl>
2022-06-02 10:53:52 +0200

    [waybar] Replace custom/kbd with sway/language

--- .config/waybar/kbd.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-IDENTIFIER="${1:-}"
-
-swaymsg -t get_inputs | jq --raw-output \
-     --arg type "keyboard" \
-     --arg identifier "${IDENTIFIER}" \
-    'map(select(contains($ARGS.named)) | .xkb_active_layout_name) | unique[]'
