0d5b9458d14457e05beae3456cbddc425c8e1aa6
Evy Bongers <evy@evybongers.nl>
2022-12-31 11:06:24 +0100

    [waybar] List arch packages in tooltip

--- .config/waybar/arch-updates.sh
+++ .config/waybar/arch-updates.sh
@@ -14,25 +14,30 @@ set -o errexit
 set -o nounset
 
 function main() {
-    local class num_repo_upgrades num_aur_upgrades
+    local class="ok"
+    local -a repo_upgrades aur_upgrades all_upgrades
 
-    num_repo_upgrades=$( (checkupdates |wc -l) || exit 0)
-    num_aur_upgrades=$( command -v yay &>/dev/null && (yay -Qua 2>/dev/null |wc -l) || true)
+    repo_upgrades=( $( (checkupdates |cut -d' ' -f1) || exit 0 ) )
+    text="Updates: ${#repo_upgrades[@]}"
 
-    text="Updates: ${num_repo_upgrades}"
-    if [[ -n "${num_aur_upgrades:-}" ]]; then
-        text+="/${num_aur_upgrades}"
+    if [[ ${+commands[yay]} ]]; then
+        aur_upgrades=( $( (yay -Qua |cut -d' ' -f1) || true ) )
+        text+="/${#aur_upgrades[@]}"
     fi
 
-    if [[ $[num_repo_upgrades+${num_aur_upgrades:-0}] -gt 0 ]]; then
+    all_upgrades=(
+        "${^repo_upgrades[@]} (repo)"
+        "${^aur_upgrades[@]} (aur)"
+    )
+
+    if [[ ${#all_upgrades[@]} -gt 0 ]]; then
         class="pending"
-    else
-        class="ok"
     fi
 
     jq -c -M -n '$ARGS.named' \
         --arg text "${text}" \
-        --arg class "${class}"
+        --arg class "${class}" \
+        --arg tooltip "${(F)all_upgrades:-All done!}"
 }
 
 main "${@}"
