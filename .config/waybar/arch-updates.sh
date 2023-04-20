ff170f473c00fdd26393413a601b0e6e91f8818a
Evy Bongers <evy@evybongers.nl>
2023-04-20 10:18:59 +0200

    [waybar] Rewrite arch-updates plugin

--- .config/waybar/arch-updates.sh
+++ .config/waybar/arch-updates.sh
@@ -2,9 +2,8 @@
 #
 # Dependencies:
 # - checkupdates (provided by pacman-contrib)
-# - jq
-# - wc (provided by coreutils)
 # - yay (optional, for checking AUR packages)
+# - jq
 if [[ "${ZSH_EVAL_CONTEXT}" != "toplevel" ]]; then
     return 1
 fi
@@ -13,31 +12,72 @@ set -o pipefail
 set -o errexit
 set -o nounset
 
-function main() {
-    local class="ok"
-    local -a repo_upgrades aur_upgrades all_upgrades
+declare -r lockfile="${XDG_RUNTIME_DIR}/arch-updates.lock"
+declare -r cache_file="${XDG_RUNTIME_DIR}/updates.json"
+declare -A update_cmds
 
-    repo_upgrades=( $( (checkupdates |cut -d' ' -f1) || exit 0 ) )
-    text="Updates: ${#repo_upgrades[@]}"
+update_cmds[repo]="checkupdates"
+update_cmds[aur]="yay -Qua"
 
-    if [[ ${+commands[yay]} ]]; then
-        aur_upgrades=( $( (yay -Qua |cut -d' ' -f1) || true ) )
-        text+="/${#aur_upgrades[@]}"
-    fi
+zmodload zsh/system
 
-    all_upgrades=(
-        "${^repo_upgrades[@]} (repo)"
-        "${^aur_upgrades[@]} (aur)"
+function fetch_updates() {
+    (
+    zsystem flock -t 0.5 "${lockfile}" || exit 0
+
+    for origin cmd in "${(kv)update_cmds[@]}"; do
+        local prog="${${(As. .)cmd}[1]}"
+        [[ ${+commands[${prog}]} -eq 1 ]] || continue
+        { "${(As. .)cmd}" 2>/dev/null || true } |
+            jq --raw-input --slurp \
+            --arg origin "${origin}" \
+            ' rtrimstr("\n")
+            | split("\n")
+            | { "\($origin)": map( split(" ")
+                                 | { "pkg": .[0],
+                                     "old": .[1],
+                                     "new": .[3]
+                                   }
+                                 )
+              }'
+    done |jq --slurp 'add' > "${cache_file}"
+
+    wait
     )
+}
 
-    if [[ ${#all_upgrades[@]} -gt 0 ]]; then
-        class="pending"
-    fi
+function main() {
+    [[ -f "${lockfile}" ]] || touch "${lockfile}"
 
-    jq -c -M -n '$ARGS.named' \
-        --arg text "${text}" \
-        --arg class "${class}" \
-        --arg tooltip "${(F)all_upgrades:-All done!}"
+    fetch_updates
+
+    zsystem flock -r "${lockfile}"
+
+    jq -c -M ' . as $updates
+             | ( "Updates: \( $updates
+                            | map(length)
+                            | join("/")
+                            )"
+               ) as $text
+             | ( if $updates |add |length > 0
+                 then "pending"
+                 else "ok"
+                 end
+               ) as $class
+             | ( $updates | to_entries
+                          | map( .key as $key
+                               | .value
+                               | map( "\(.pkg) (\($key))" )
+                               )
+                          | add
+                          | join("\n")
+                          | if . == "" then "All done!" else . end
+               ) as $tooltip
+             | { "text": $text
+               , "class": $class
+               , "tooltip": $tooltip
+               }
+             ' "${cache_file}"
 }
 
 main "${@}"
